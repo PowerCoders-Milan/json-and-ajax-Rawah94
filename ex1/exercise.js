361ade44e993022dfa7f9b36831408c21b081b49
Rawah G <rawah.h@outlook.com>
2021-03-09 13:21:31 +0100

    updated?

---
+++ ex1/exercise.js
@@ -0,0 +1,13 @@
+let object = {
+    id: 1,
+    title: "this is a note",
+    content: "This is the amazing content of the note!"
+  };
+
+object.subTitle = "my note";
+console.log(object);
+let myJSON = JSON.stringify(object);
+console.log(myJSON);
+
+const myObject = JSON.parse(myJSON);
+console.log(myObject);
